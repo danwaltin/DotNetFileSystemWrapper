024a26ca544c819ab2d0b734b19cd4234e9749f8
Dan Waltin <dan@waltin.se>
2020-11-30 21:06:03 +0100

    moved root path, and clear root path after tests

--- DotNetFileSystemWrapper.Tests/File/Exists.cs
+++ DotNetFileSystemWrapper.Tests/File/Exists.cs
@@ -1,12 +1,31 @@
 using Xunit;
+using System;
 using DotNetFileSystemWrapper;
 
 namespace DotNetFileSystemWrapper.Tests.File
 {
-    public class ExistsTests {
-        private string RootPath() {
-            
-            return "/Users/dan/";
+    public class ExistsTests : IDisposable {
+        private IFileSystem _fs;
+
+        public ExistsTests() {
+            AssureRootPathExists();
+            _fs = new FileSystemFactory().PhysicalFileSystem();
+        }
+
+        public void Dispose() {
+            ClearRootPath();
+        }
+
+        private static string RootPath() {
+            return System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "TestRoot");
+        }
+
+        private static void AssureRootPathExists() {
+            System.IO.Directory.CreateDirectory(RootPath());            
+        }
+
+        private static void ClearRootPath() {
+            System.IO.Directory.Delete(RootPath(), recursive: true);
         }
 
         private string PathRelativeToRoot(string filename) {
@@ -15,20 +34,16 @@ namespace DotNetFileSystemWrapper.Tests.File
 
         [Fact]
         public void NonExistingFileShouldReturnFalse() {
-            var fileSystem = new FileSystemFactory().PhysicalFileSystem();
-
-            var exists = fileSystem.File.Exists("filename_of_non_existing_file.txt");
+            var exists = _fs.File.Exists("filename_of_non_existing_file.txt");
 
             Assert.False(exists);
         }
 
         [Fact]
         public void ExistingFileShouldReturnTrue() {
-            var fileSystem = new FileSystemFactory().PhysicalFileSystem();
-
             var path = PathRelativeToRoot("file_name.txt");
-            fileSystem.File.WriteAllText(path, "content");
-            var exists = fileSystem.File.Exists(path);
+            _fs.File.WriteAllText(path, "content");
+            var exists = _fs.File.Exists(path);
 
             Assert.True(exists);
         }
