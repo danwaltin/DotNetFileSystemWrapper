b24928eddfc73fd88727e646e132bd558c437dd4
Dan Waltin <dan@waltin.se>
2020-11-30 08:33:21 +0100

    in the middle

--- tests/File/Exists.cs
+++ tests/File/Exists.cs
@@ -4,6 +4,15 @@ using src;
 namespace tests.File
 {
     public class ExistsTests {
+        private string RootPath() {
+            
+            return "/Users/dan/";
+        }
+
+        private string PathRelativeToRoot(string filename) {
+            return System.IO.Path.Combine(RootPath(), filename);
+        }
+
         [Fact]
         public void NonExistingFileShouldReturnFalse() {
             var fileSystem = new FileSystemFactory().PhysicalFileSystem();
@@ -12,5 +21,18 @@ namespace tests.File
 
             Assert.False(exists);
         }
+
+        [Fact]
+        public void ExistingFileShouldReturnTrue() {
+            var fileSystem = new FileSystemFactory().PhysicalFileSystem();
+
+            var path = PathRelativeToRoot("file_name.txt");
+            fileSystem.File.WriteAllText(path, "content");
+            var exists = fileSystem.File.Exists(path);
+
+            Assert.True(exists);
+
+        }
+
     }
 }
